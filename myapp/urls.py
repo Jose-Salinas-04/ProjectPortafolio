1e15f96295ce81f5df86a0edc0ae2e013e69d9a2
jose <jsalinasbadilla4@gmail.com>
2024-06-21 18:44:47 -0400

    url

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index),
-    path('agendar', views.agendar),
-    path('nosotros', views.nosotros),
-    path('contactanos', views.contactanos)
+    path('', views.index, name='index'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('contactanos/', views.contactanos, name='contactanos'),
+    path('fechayhora/', views.fechayhora, name='fechayhora'),
+    path('reservar/', views.reservar, name='reservar'),  # Asegúrate de que esta línea exista
 ]
+
