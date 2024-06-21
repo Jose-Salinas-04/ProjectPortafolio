ca4effe76eea6461073fb041cac725f064ee483a
jose <jsalinasbadilla4@gmail.com>
2024-06-21 18:41:38 -0400

    archivo

---
+++ myapp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Cita, Usuario
+
+class ReservaForm(forms.ModelForm):
+    usuario_nombre = forms.CharField(max_length=50)
+    usuario_correo = forms.EmailField()
+    usuario_telefono = forms.CharField(max_length=15)
+
+    class Meta:
+        model = Cita
+        fields = ['barbero', 'tipo_corte', 'fecha_hora', 'duracion']
+
+    def save(self, commit=True):
+        usuario_data = {
+            'nombre': self.cleaned_data.pop('usuario_nombre'),
+            'correo': self.cleaned_data.pop('usuario_correo'),
+            'telefono': self.cleaned_data.pop('usuario_telefono')
+        }
+        usuario, created = Usuario.objects.get_or_create(correo=usuario_data['correo'], defaults=usuario_data)
+        cita = super().save(commit=False)
+        cita.usuario = usuario
+        if commit:
+            cita.save()
+        return cita
