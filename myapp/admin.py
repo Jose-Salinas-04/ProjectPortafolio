3350857040aaa7647bb465ef3bc1904bdd9df742
jose <jsalinasbadilla4@gmail.com>
2024-06-21 19:24:42 -0400

    probando los commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Barbero
+from .models import Usuario
 
-# Register your models here.
+# Register your models here
+admin.site.register(Barbero)
+admin.site.register(Usuario )
+
