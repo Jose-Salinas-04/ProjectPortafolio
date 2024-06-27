d2aa88e66aa7878ab55b5dc14149d1d532eee8bc
jose <jsalinasbadilla4@gmail.com>
2024-06-26 20:32:08 -0400

    Actualizacion

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,7 +3,11 @@ from .models import Barbero
 from .models import Usuario
 from .models import Cita
 from .models import TipoCorte
+from .models import Comentario
+
 
 # Register your models here
 admin.site.register(Barbero)
 admin.site.register(Usuario)
+admin.site.register(TipoCorte)
+admin.site.register(Comentario)
